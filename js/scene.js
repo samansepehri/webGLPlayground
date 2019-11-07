4e0110b57912f9cc0a3cdf938dd8e451d4a7e9ca
samansepehri <samansepehri@gmail.com>
2019-11-06 18:15:25 -0700

    updates

--- js/scene.js
+++ js/scene.js
@@ -1,6 +1,30 @@
+import * as THREE from './three.module.js';
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var sceneWidth = window.innerWidth / 2;
+var sceneHeight = window.innerHeight / 2;
+
+var camera = new THREE.PerspectiveCamera( 75, sceneWidth / sceneHeight, 0.1, 1000 );
+
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( sceneWidth, sceneHeight );
 document.body.appendChild( renderer.domElement );
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+animate();
+
+function animate() {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+    
+    renderer.render( scene, camera );
+}
